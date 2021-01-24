16f42500c1a85df2558ce370b202e2f9a1ad4ee4
volmk <lvi.work1@gmail.com>
2021-01-24 14:57:07 +0200

    init

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld v-bind:msg="message" />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    message: "",
+  }),
+  async created() {
+    const {data} = await axios.get(`/api`);
+    this.message = data;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
